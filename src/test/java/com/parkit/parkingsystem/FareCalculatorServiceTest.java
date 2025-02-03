d32b1ff256a30e39373235db47d31c094ebdd385
Pierce Broudin <piercebroudin.pb@gmail.com>
2025-02-03 12:24:09 +0100

    discount tests added

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -161,4 +161,41 @@ public class FareCalculatorServiceTest {
 
         assertEquals(0, ticket.getPrice());
     }
+
+    @Test
+    public void calculateFareCarWithDiscount() {
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - (60 * 60 * 1000) );
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+
+        fareCalculatorService.calculateFare(ticket, true);
+
+        double expectedFare = Fare.CAR_RATE_PER_HOUR * 0.95;
+        assertEquals(expectedFare, ticket.getPrice(), 0.001);
+
+    }
+
+    @Test
+    public void calculateFareBikeWithDiscount() {
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - (60 * 60 * 1000) );
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE,false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+
+        fareCalculatorService.calculateFare(ticket, true);
+
+        double expectedFare = Fare.BIKE_RATE_PER_HOUR * 0.95;
+        assertEquals(expectedFare, ticket.getPrice(), 0.001);
+
+    }
+
 }
