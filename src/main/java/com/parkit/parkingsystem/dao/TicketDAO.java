3b1e3b6c693db94d322b2bc2e768cc358f87e0bb
Pierce Broudin <piercebroudin.pb@gmail.com>
2025-02-03 15:53:10 +0100

    modified processIncoming/ExitingVehicle methods

--- src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
@@ -86,4 +86,31 @@ public class TicketDAO {
         }
         return false;
     }
+
+    public int getNbTicket(String vehicleRegNumber) {
+        Connection con = null;
+        int nbTicket = 0;
+
+        try {
+            con = dataBaseConfig.getConnection();
+            PreparedStatement ps = con.prepareStatement(
+                    "SELECT COUNT (*) FROM ticket WHERE VEHICLE_REG_NUMBER = ?"
+            );
+            ps.setString(1, vehicleRegNumber);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                nbTicket = rs.getInt(1);
+            }
+
+            dataBaseConfig.closeResultSet(rs);
+            dataBaseConfig.closePreparedStatement(ps);
+        } catch (Exception ex) {
+            logger.error("Error counting tickets for vehicle : " +  vehicleRegNumber,ex);
+        } finally {
+            dataBaseConfig.closeConnection(con);
+        }
+        return nbTicket;
+    }
 }
--- src/main/java/com/parkit/parkingsystem/service/ParkingService.java
+++ src/main/java/com/parkit/parkingsystem/service/ParkingService.java
@@ -32,6 +32,15 @@ public class ParkingService {
             ParkingSpot parkingSpot = getNextParkingNumberIfAvailable();
             if(parkingSpot !=null && parkingSpot.getId() > 0){
                 String vehicleRegNumber = getVehichleRegNumber();
+
+                int nbTickets = ticketDAO.getNbTicket(vehicleRegNumber);
+                if (nbTickets > 0) {
+                    System.out.println("Heureux de vous revoir ! En tant qu'utilisateur régulier" +
+                            "de notre parking, vous allez obtenir une remise de 5%.");
+                } else {
+                    System.out.println("Bienvenue chez Parkit! Nous sommes heureux de vous accueillir");
+                }
+
                 parkingSpot.setAvailable(false);
                 parkingSpotDAO.updateParking(parkingSpot);//allot this parking space and mark it's availability as false
 
@@ -103,7 +112,11 @@ public class ParkingService {
             Ticket ticket = ticketDAO.getTicket(vehicleRegNumber);
             Date outTime = new Date();
             ticket.setOutTime(outTime);
-            fareCalculatorService.calculateFare(ticket);
+
+            boolean discount = ticketDAO.getNbTicket(vehicleRegNumber) > 1;
+
+            fareCalculatorService.calculateFare(ticket, discount);
+
             if(ticketDAO.updateTicket(ticket)) {
                 ParkingSpot parkingSpot = ticket.getParkingSpot();
                 parkingSpot.setAvailable(true);
